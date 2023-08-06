7e75b3344316bc729e8cca47b1bfc4e7dd63c7c4
Cathret <cathret@users.noreply.github.com>
2023-08-06 19:38:02 +0200

    Log Settings now set to Warning instead of Debug.

--- TK2CommunityBot/Settings.cs
+++ TK2CommunityBot/Settings.cs
@@ -8,7 +8,7 @@ namespace TK2Bot
         public static readonly string BOT_TOKEN = Environment.GetEnvironmentVariable("TK2_BOT_TOKEN")!;
         
         public const string COMMAND_PREFIX      = "!tk2 ";
-        public const LogLevel LOG_LEVEL         = LogLevel.Debug;
+        public const LogLevel LOG_LEVEL         = LogLevel.Warning;
         public const DiscordIntents INTENTS     = DiscordIntents.Guilds | DiscordIntents.GuildMessageTyping | DiscordIntents.GuildMessages | DiscordIntents.GuildWebhooks | DiscordIntents.GuildIntegrations | DiscordIntents.MessageContents;
     }
 }
