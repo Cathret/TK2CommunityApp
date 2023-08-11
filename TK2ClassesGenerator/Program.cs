ae18943a026265e24f099ae5f57fba6ffcd56963
Cathret <cathret@users.noreply.github.com>
2023-08-11 16:49:45 +0200

    Add new Project ClassesGenerator.
    This project will be run to get information from CSV files and generate .cs files for use in the TK2CommunityBot.

---
+++ TK2ClassesGenerator/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TK2Bot.ClassesGenerator
+{
+    internal static class Program
+    {
+        private static int Main(string[] _args)
+        {
+            return 0;
+        }
+    }
+}
