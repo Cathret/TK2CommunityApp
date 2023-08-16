9344db48661000433b1d50a0c959c3345e2fa4bb
Cathret <cathret@users.noreply.github.com>
2023-08-16 16:25:40 +0200

    MessageGenerator_WorldRecord now sends different messages depending on the filter.

--- TK2CommunityBot/TK2Bot/MessageGenerators/MessageGenerator_WorldRecord.cs
+++ TK2CommunityBot/TK2Bot/MessageGenerators/MessageGenerator_WorldRecord.cs
@@ -5,13 +5,35 @@ namespace TK2Bot
 {
     public static partial class MessageGenerator
     {
-        public static async Task<DiscordMessageBuilder> CreateWrMessage(ETrackId _trackId)
+        public static async Task<DiscordMessageBuilder> CreateWrMessage(ETrackId _trackId, ELocation _location)
         {
-            PlayerTrackTime worldRecord = await ApiSystem.GetWorldRecordForTrack(_trackId);
+            if (LocationUtils.IsValidOption(_location) == false)
+            {
+                return new DiscordMessageBuilder()
+                    .WithContent("Invalid Filter.");
+            }
+            
+            PlayerTrackTime worldRecord = await ApiSystem.GetWorldRecordForTrack(_trackId, _location);
             PlayerInfo playerInfo = worldRecord.PlayerInfo;
             TrackInfo trackInfo = worldRecord.TrackInfo;
 
             string formattedDuration = worldRecord.RunTime.ToString(TIMER_FORMAT);
+
+            string locationRecordTitle = "World Record";
+            string locationRecordDesc = "WR";
+            // TODO: use single GetEmoji function instead of splitting country/continent
+            if ((_location & ELocation.COUNTRY) != ELocation.NO_FILTER)
+            {
+                locationRecordTitle = $"{LocationUtils.GetLocationNameFromLocationEnum(_location)}'s Record";
+                string countryFlag = $":flag_{LocationUtils.GetAlias(_location)}:";
+                locationRecordDesc = $"{countryFlag}'s record";
+            }
+            else if ((_location & ELocation.CONTINENT) != ELocation.NO_FILTER)
+            {
+                locationRecordTitle = $"{LocationUtils.GetLocationNameFromLocationEnum(_location)}'s Record";
+                string continentEmoji = RankingUtils.GetContinentEmojiTmp(LocationUtils.GetAlias(_location).ToUpper());
+                locationRecordDesc = $"{continentEmoji}'s record";
+            }
 
             DiscordEmbedBuilder embedBuilder = new DiscordEmbedBuilder()
             {
@@ -21,8 +43,8 @@ namespace TK2Bot
                     Url     = playerInfo.ProfileUrl,
                     IconUrl = playerInfo.AvatarUrl,
                 },
-                Title       = $"{trackInfo.MapName}'s World Record",
-                Description = $"Current WR is held by **{playerInfo.PlayerName}** with a time of **{formattedDuration}**.",
+                Title       = $"{trackInfo.MapName}'s {locationRecordTitle}",
+                Description = $"Current {locationRecordDesc} is held by **{playerInfo.PlayerName}** with a time of **{formattedDuration}**.",
                 Url         = trackInfo.LeaderboardUrl,
                 Timestamp   = DateTimeOffset.UtcNow,
                 Color       = DiscordColor.Blue,
