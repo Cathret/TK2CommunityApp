9fed35ce98771d511d5118af46a6dee4c69dfe22
Cathret <cathret@users.noreply.github.com>
2023-08-18 14:51:24 +0200

    Added Leaderboards and LeaderboardsRecord to API Containers.
    Those will permit to retrieve information on Leaderboards.

---
+++ TK2CommunityBot/TK2Api/Containers/Leaderboards.cs
@@ -0,0 +1,9 @@
+﻿namespace TK2Bot.API
+{
+    public struct Leaderboards
+    {
+        public TrackInfo TrackInfo { get; internal init; }
+        
+        public LeaderboardRecord[] LeaderboardRecords { get; internal init; }
+    }
+}
