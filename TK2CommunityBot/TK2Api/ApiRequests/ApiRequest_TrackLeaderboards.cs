d38039533e417ee10b465c7364b84e7cd94221c6
Cathret <cathret@users.noreply.github.com>
2023-08-18 18:52:59 +0200

    Splitted ApiSystem for every request to be made.

---
+++ TK2CommunityBot/TK2Api/ApiRequests/ApiRequest_TrackLeaderboards.cs
@@ -0,0 +1,66 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace TK2Bot.API
+{
+    public static partial class ApiSystem
+    {
+        public static async Task<Leaderboards> GetTrackLeaderboards(ETrackId _trackId, ELocation _location)
+        {
+            string mapSlug = MapTranslator.GetSlugFromTrackId(_trackId);
+
+            string requestUri = $"track/{mapSlug}/leaderboard{GetLocationFilterOptions(_location)}";
+
+            string contentAsString = await HTTP_CLIENT.GetStringAsync(requestUri);
+            
+            dynamic contentAsJson = JObject.Parse(contentAsString);
+            
+            List<LeaderboardRecord> allLeaderboardRecords = new List<LeaderboardRecord>();
+            foreach (dynamic oneRecord in contentAsJson.data.records)
+            {
+                allLeaderboardRecords.Add(new LeaderboardRecord()
+                    {
+                        PlayerInfo = new PlayerInfo()
+                        {
+                            PlayerName = oneRecord.player.name,
+                            ProfileUrl = oneRecord.player.profile_url,
+                            AvatarUrl  = oneRecord.player.avatar_url
+                        },
+                        CountryInfo = new CountryInfo()
+                        {
+                            Name     = oneRecord.player.country.name,
+                            Alias    = oneRecord.player.country.alpha2,
+                            ImageUrl = oneRecord.player.country.image_url,
+                        },
+                        ContinentInfo = new ContinentInfo()
+                        {
+                            Name     = oneRecord.player.continent.name,
+                            Alias    = oneRecord.player.continent.alpha2,
+                            ImageUrl = oneRecord.player.continent.image_url,
+                        },
+                        PlayerStats = new PlayerStats()
+                        {
+                            PosWorldwide = oneRecord.data.positions.worldwide,
+                            PosContinent = oneRecord.data.positions.continent,
+                            PosCountry   = oneRecord.data.positions.country,
+                            Points       = oneRecord.data.points
+                        },
+                        RunTime  = TimeSpan.FromMilliseconds(double.Parse(oneRecord.score.ToString())),
+                    }
+                );
+            }
+
+            Leaderboards leaderboards = new Leaderboards()
+            {
+                TrackInfo = new TrackInfo()
+                {
+                    MapName        = contentAsJson.data.track.name,
+                    ImageUrl       = contentAsJson.data.track.image_url,
+                    LeaderboardUrl = contentAsJson.data.track.leaderboard_url,
+                },
+                LeaderboardRecords = allLeaderboardRecords.ToArray(),
+            };
+
+            return leaderboards;
+        }
+    }
+}
---
+++ TK2CommunityBot/TK2Api/ApiRequests/ApiRequest_WorldRecord.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json.Linq;
+
+namespace TK2Bot.API
+{
+    public static partial class ApiSystem
+    {
+        public static async Task<PlayerTrackTime> GetWorldRecordForTrack(ETrackId _trackId, ELocation _location)
+        {
+            string mapSlug = MapTranslator.GetSlugFromTrackId(_trackId);
+
+            string requestUri = $"track/{mapSlug}/world-record{GetLocationFilterOptions(_location)}";
+
+            string contentAsString = await HTTP_CLIENT.GetStringAsync(requestUri);
+            
+            dynamic contentAsJson = JObject.Parse(contentAsString);
+
+            PlayerInfo wrHolderInfo = new PlayerInfo()
+            {
+                PlayerName = contentAsJson.data.record.player.name,
+                ProfileUrl = contentAsJson.data.record.player.profile_url,
+                AvatarUrl  = contentAsJson.data.record.player.avatar_url, 
+            };
+
+            TrackInfo trackInfo = new TrackInfo()
+            {
+                MapName        = contentAsJson.data.track.name,
+                ImageUrl       = contentAsJson.data.track.image_url,
+                LeaderboardUrl = contentAsJson.data.track.leaderboard_url,
+            };
+
+            PlayerTrackTime wrTrackTime = new PlayerTrackTime()
+            {
+                PlayerInfo  = wrHolderInfo,
+                TrackInfo   = trackInfo,
+                RunTime     = TimeSpan.FromMilliseconds(double.Parse(contentAsJson.data.record.score.ToString())),
+                PlayerStats = new PlayerStats(),
+            };
+
+            return wrTrackTime;
+        }
+    }
+}
