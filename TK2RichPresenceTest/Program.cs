df388d81541d4a68d3e1b13d0cc2657d47016c9f
Cathret <cathret@users.noreply.github.com>
2023-11-07 00:50:48 +0100

    Finished testing Rich Presence using a custom DiscordHandler. Everything is done without any "Program input", only using Discord input.
    The objective is to be able to have a "flow" example.
    
    Having a proper system in game to be able to handle Activity updates, manual invites/join and actually join the games through "secrets" should work properly.

--- TK2RichPresenceTest/Program.cs
+++ TK2RichPresenceTest/Program.cs
@@ -2,19 +2,50 @@
 {
     internal static class Program
     {
-        private const int WAIT_TIMER_MS = 1000 / 60;
-        private static readonly string BOT_CLIENT_ID = Environment.GetEnvironmentVariable("TK2_BOT_CLIENT_ID")!;
+        private const bool DEBUG_DUAL_DISCORD = false;
 
+        private const int WAIT_TIMER_MS = 1000 / 60;
+        private const uint STEAM_APP_ID = 2269950;
+        
+        private static readonly string BOT_CLIENT_ID = Environment.GetEnvironmentVariable("TK2_BOT_CLIENT_ID")!;
+        
         private static int Main()
         {
+            return DEBUG_DUAL_DISCORD ? DebugDualDiscord() : RunSingleDiscord();
+        }
+
+        private static int RunSingleDiscord()
+        {
             DiscordHandler discordHandler = new DiscordHandler(long.Parse(BOT_CLIENT_ID));
-            
-            discordHandler.Initialize();
+            discordHandler.Initialize(STEAM_APP_ID);
 
             bool updateSuccess = true;
             while (updateSuccess)
             {
                 updateSuccess = discordHandler.RichPresenceUpdate();
+                Thread.Sleep(WAIT_TIMER_MS);
+            }
+
+            return 0;
+        }
+
+        private static int DebugDualDiscord()
+        {
+            Environment.SetEnvironmentVariable("DISCORD_INSTANCE_ID", "0");
+            DiscordHandler discordStable = new DiscordHandler(long.Parse(BOT_CLIENT_ID));
+            discordStable.Initialize(STEAM_APP_ID);
+
+            Environment.SetEnvironmentVariable("DISCORD_INSTANCE_ID", "1");
+            DiscordHandler discordCanary = new DiscordHandler(long.Parse(BOT_CLIENT_ID));
+            discordCanary.Initialize(STEAM_APP_ID);
+
+            bool updateStableSuccess = true;
+            bool updateCanarySuccess = true;
+            while (updateStableSuccess && updateCanarySuccess)
+            {
+                updateStableSuccess = discordStable.RichPresenceUpdate();
+                updateCanarySuccess = discordCanary.RichPresenceUpdate();
+                
                 Thread.Sleep(WAIT_TIMER_MS);
             }
 
