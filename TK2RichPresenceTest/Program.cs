ad141ab22d54a279af373e220c5e91b3409eaab9
Cathret <cathret@users.noreply.github.com>
2023-11-06 17:22:00 +0100

    Created new Project to handle RichPresence testing.

---
+++ TK2RichPresenceTest/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace TK2Bot.TK2RichPresenceTest
+{
+    internal static class Program
+    {
+        private static readonly string BOT_CLIENT_ID = Environment.GetEnvironmentVariable("TK2_BOT_CLIENT_ID")!;
+
+        private static async Task Main()
+        {
+            DiscordHandler discordHandler = new DiscordHandler(long.Parse(BOT_CLIENT_ID));
+            
+            discordHandler.Initialize();
+
+            bool updateSuccess = true;
+            while (updateSuccess)
+            {
+                updateSuccess = discordHandler.RichPresenceUpdate();
+                await Task.Delay(1000 / 60);
+            }
+        }
+    }
+}
